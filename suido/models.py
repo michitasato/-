31fcb91891b8229373b275d3bbaa23560d7f88ca
michitasato <nezimakidorizenmai@yahoo.co.jp>
2019-01-09 13:01:45 +0900

    テーブル作成と管理者画面の追加

--- suido/models.py
+++ suido/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Data(models.Model):
+    account_date = models.DateField()
+    shop = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    account = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return 'ID:' + str(self.id) + ',' + str(self.account_date) + ',' + self.shop + ',' +  \
+            self.category + ',' + str(self.account) + '円'
+
--- suido/views.py
+++ suido/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Data
 
 def index(request):
+    data = Data.objects.all()
     params = {
-            'title': '最初のページ',
-            'msg': 'これはサンプルページです。',      
+            'title': '最初のレコード表示',
+            'message': 'これはサンプルページです。',
+            'data': data
     } 
     
     return render(request, 'index.html', params)
