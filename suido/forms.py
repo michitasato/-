60eb9d0295b9af5d1be712d7c6326c5d8d8a1102
michitasato <nezimakidorizenmai@yahoo.co.jp>
2019-01-11 13:46:53 +0900

    add form

--- suido/forms.py
+++ suido/forms.py
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
+from django import forms
 
+# レコードを検索するフォーム
+class FindForm(forms.Form):
+    shop_data=[
+            ('松茂店', '松茂店'),
+            ('羽ノ浦店', '羽ノ浦店'),
+            ]
+    category_data=[
+            ('電気代', '電気代'),
+            ('水道代', '水道代'),
+            ('ガス代', 'ガス代')
+            ]
+    start_date = forms.DateField(label='開始年月', required=False)
+    end_date = forms.DateField(label='終了年月', required=False)
+    shop = forms.MultipleChoiceField(label='店舗名', \
+          choices=shop_data, widget=forms.SelectMultiple(attrs={'size':3}))
+    category = forms.MultipleChoiceField(label='分類', \
+          choices=category_data, widget=forms.SelectMultiple(attrs={'size':3}))
+    
--- suido/views.py
+++ suido/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
-from .models import Data
+from django.views.generic import View
 
-def index(request):
-    data = Data.objects.all()
-    params = {
-            'title': '最初のレコード表示',
-            'message': 'これはサンプルページです。',
-            'data': data
-    } 
+from .models import Data
+from .forms import FindForm
+
+# index関数　トップページ 　条件を指定する前の状態
+class Index(View):    
+    def get(self, request, *args, **kwargs):
+        # GETリクエスト用のメソッド
+        data = Data.objects.all().order_by('account_date').reverse()
+        params = {
+                'form': FindForm(),
+                'data': data,
+                } 
+        # トップ画面用のテンプレートに値がからの空のフォームをレンダリングする
+        return render(request, 'index.html', params)
     
-    return render(request, 'index.html', params)
+    def post(self, request, *args, **kwargs):
+            # POSTリクエスト用のメソッド
+        data = Data.objects\
+        .filter(account_date__gte = request.POST['start_date'],\
+                account_date__lte = request.POST['end_date'])\
+                .filter(shop = request.POST['shop'])\
+                .filter(category = request.POST['category'])\
+                .order_by('account_date')\
+                .reverse()
+
+        params = {
+                'form':FindForm(request.POST),
+                'data': data,
+                }
+        return render(request, 'index.html', params)
